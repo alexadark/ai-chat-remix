69180880d41fee16fe4f8339cdb8104f0b475a2a
Alexandra Spalato <alexaspalato@gmail.com>
2024-05-03 16:16:39 +0200

    flat routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
+import { flatRoutes } from "remix-flat-routes";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -9,5 +10,13 @@ export default defineConfig({
   server: {
     port: 3000,
   },
-  plugins: [remix(), tsconfigPaths()],
+  plugins: [
+    remix({
+      ignoredRouteFiles: ["**/*"],
+      routes: async (defineRoutes) => {
+        return flatRoutes("routes", defineRoutes);
+      },
+    }),
+    tsconfigPaths(),
+  ],
 });
